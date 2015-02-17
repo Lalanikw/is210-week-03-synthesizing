ed44e7402551291a9e126740dab4366afceaff54
Lalani Werake <lalanikw@gmail.com>
2015-02-17 05:24:25 +0000

    task_04.py

--- task_04.py
+++ task_04.py
@@ -6,3 +6,5 @@ NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1}!'
 FNAME = 'Pat'
 NTYPE = '*amazing*'
 RNUM = 42
+EMAIL = 'Hi {person}! I have {0} news! I won the raffle with number {1}{2}!'.format(NTYPE, 00, RNUM, person='Pat')
+print EMAIL
